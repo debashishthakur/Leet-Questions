abaa50c714c1db4c53b047f5e4d55de843ea9a99
DEBASHISH THAKUR <53852657+debashishthakur@users.noreply.github.com>
2022-09-20 00:13:17 +0530

    Update Two Sum.py

--- Arrays and Hashing/Two Sum.py
+++ Arrays and Hashing/Two Sum.py
@@ -1,8 +1,9 @@
-def twoSum(self, nums, target):
-    hm = {}
-    for i,n in enumerate nums:
-    diff = target - n
-    if diff in hm:
-        return hm[diff],i
-    else:
-        hm[n] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in hm:
+                return hm[diff], i
+            else:
+                hm[n] = i                                                                                                                                                                                                                                                                                                                                                           
