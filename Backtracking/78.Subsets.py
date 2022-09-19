93ab49e8296671b615eca2395420c34010e967dd
DEBASHISH THAKUR <53852657+debashishthakur@users.noreply.github.com>
2022-09-20 00:12:34 +0530

    Create 78.Subsets.py

---
+++ Backtracking/78.Subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = [[]]
+        
+        for i in nums:
+            output += [lst + [i] for lst in output]
+            
+        return output
+                
