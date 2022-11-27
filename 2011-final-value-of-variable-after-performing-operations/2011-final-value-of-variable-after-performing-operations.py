f672d4360964f1001b020cfbeb692c07be2e3a2c
DEBASHISH THAKUR <53852657+debashishthakur@users.noreply.github.com>
2022-11-27 09:35:02 +0530

    Time: 125 ms (20.70%), Space: 13.8 MB (96.37%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -3,9 +3,11 @@ class Solution:
         #operations = list(map(str, input().split()))
         x = 0
         for i in operations:
-            if i == "++X" or i == "X++":
-                x += 1
-            elif i == "--X" or i =="X--":
+            if i[1] == "-":
                 x -= 1
+            elif i[1] == "+":
+                x += 1
         return x
+            
+            
                 
